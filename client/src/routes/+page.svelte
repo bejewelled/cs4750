1f953300b33bac2de34c620ebed58925b7c1910d
bejewelled <jtg7sz@virginia.edu>
2023-10-31 18:01:22 -0400

    add frontend skeleton

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -5,27 +5,11 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Recipes</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div>Search and Create Recipes</div>
 </section>
 
 <style>
---
+++ client/src/routes/search/+page.svelte
@@ -0,0 +1,46 @@
+<div class="text-column flexbox">
+	<h2 class='text-center w-full text-gray-300'>
+		Search for Recipes
+	</h2>
+
+	<form on:submit={handleSubmit}>
+		<label for="name">Name:</label>
+		<input type="text" id="name" bind:value={name} />
+	  
+		<label for="email">Email:</label>
+		<input type="email" id="email" bind:value={email} />
+	  
+		<button type="submit">Submit</button>
+	  </form>
+
+	<button on:click={doPost()}>
+	post
+	</button>
+
+	
+</div>
+
+<script>
+let name = '';
+let email = '';
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const data = { name, email };
+
+  const response = await fetch('/api/your-endpoint', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    // Handle a successful response (e.g., show a success message)
+  } else {
+    // Handle errors (e.g., show an error message)
+  }
+}
+</script>
