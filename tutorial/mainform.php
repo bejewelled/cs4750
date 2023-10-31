254e653ecbc39f3d4b5c811927f2f2da6dda7256
bejewelled <jtg7sz@virginia.edu>
2023-10-31 17:20:58 -0400

    changes

--- tutorial/mainform.php
+++
@@ -1,6 +0,0 @@
-<form name="mainForm" action="form-processing.php" method="post">   
-  <div class="row mb-3 mx-3">
-    Your name:
-    <input type="text" class="form-control" name="name" required />        
-  </div>  
-</form>  
